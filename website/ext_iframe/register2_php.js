2da32e9126ed16d15f00aea9883cb7df83245b97
Luhte <94922511+Luhte@users.noreply.github.com>
2023-11-14 08:21:24 +0200

    Add files via upload

---
+++ website/ext_iframe/register2_php.js
@@ -0,0 +1,17 @@
+var com_adswizz_register_PROTOCOL_VERSION = '2.2.0';
+
+if (typeof com_adswizz_synchro_listenerid === 'undefined') {
+    var com_adswizz_synchro_listenerid = '62fa4f09dc98feee0d0f8d8b574bf09b';
+}
+else {
+    com_adswizz_synchro_listenerid = '62fa4f09dc98feee0d0f8d8b574bf09b';
+}
+
+if (typeof com_adswizz_synchro_listnerid === 'undefined') {
+    var com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+else {
+    com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+var aw_0_req_gdpr = false;
+var us_privacy = '';
